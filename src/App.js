ed10145702aa507fc1610ef4a39c0e3f913b340b
Mario Alves <mlcalves@gmail.com>
2023-05-25 07:13:17 +0100

    atualização 25-05-2023

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="lines" bg={true} className='particles-bg-canvas-self' />
       <Navigation />
       <Logo />
-      {/* 
+      <Rank />
       <ImageLinkForm />
+      {/* 
+      
       <FaceRecognition />} */}
     </div>
   );
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt>
-                <div style={{ height: '150px', width: '150px', backgroundColor: 'darkgreen' }}>
-                    <h1>React Parallax Tilt 👀</h1>
+            <Tilt className='Tilt'>
+                {/* <div style={{ height: '150px', width: '150px', backgroundColor: 'darkgreen' }}> */}
+                <div className='Tilt'>
+                    <div className='pa3'>
+                        <img style={{ paddingTop: '5px' }} alt='logo' src={brain} />
+                    </div>
+
                 </div>
             </Tilt>
         </div>
